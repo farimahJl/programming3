297043593e711d1715c55bed7b99da5bf749e3c4
farimahJl <marry.jlal@gmail.com>
2022-09-13 17:29:12 +0200

    added slurm scheduling

--- Assignment3/Assignment3.sh
+++ Assignment3/Assignment3.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#SBATCH --time 7:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=blastquery_farimah
+#SBATCH --output=outputfile
+#SBATCH --partition=assemblix
 export BLASTDB=/local-fs/datasets/
 
 mkdir -p output
@@ -8,3 +14,4 @@ do
     /usr/bin/time -a -o output/timings.txt -f "${n}\t%e"  blastp -query MCRA.faa -db $BLASTDB/refseq_protein/refseq_protein -num_threads $n -outfmt 6 >> output/blastoutput.txt
 done
 
+python3 assignment3.py
